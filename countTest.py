a37a30e580ab670c2f3e1268d460c43d2aa36cba
Thomas Bowen <thomasbowen@btinternet.com>
2016-04-02 15:20:18 +0100

    addedmorequestions

--- countTest.py
+++ countTest.py
@@ -1,24 +1,11 @@
-import randomQuestionGenerator as rQG
+import randomCountingQuestionGenerator as rQG
+import countQuestGet as QG
 from tkinter import *
 from random import shuffle
 
 
 
-
-# print(Q1A)
-# if Q1[0] == "C":
-#     print("C(" + str(Q1[1]) + ", " + str(Q1[2]) + ") = :")
-#     for i in Q1A:
-#         print (i)
-#
-# elif Q1[0] == "P":
-#     print("P(" + str(Q1[1]) + ", " + str(Q1[2]) + ") = :")
-#     for i in Q1A:
-#         print(i)
-
-
-
-class test(object):
+class randomQuest(object):
     def main(self):
         self.frame = Frame(root)
         self.frame.pack()
@@ -40,16 +27,15 @@ class test(object):
         R4 = Radiobutton(self.frame, text=str(self.Q1A[3]), variable=self.intVar, value=self.Q1A[3])
         R4.pack(anchor=W)
 
-        submit = Button(self.frame, text="Submit", command=self.sel)
+        submit = Button(self.frame, text="Submit", command=self.selr)
         submit.pack()
 
         self.labelOpt = Label(self.frame)
         self.labelOpt.pack()
-        reset = Button(self.frame, text = "Reset", command = self.res)
-        reset.pack()
 
 
-    def sel(self):
+
+    def selr(self):
         if self.Q1[0] == "C":
             answer = rQG.comboAns(self.Q1[1], self.Q1[2])
         elif self.Q1[0] == "P":
@@ -70,17 +56,64 @@ class test(object):
         else:
             self.strVar.set("ERROR!!")
 
-    def res(self):
-        self.frame.destroy()
-        test().main()
+class Quest(object):
+    def main(self, number):
+        self.number = number
+        self.frame = Frame(root)
+        self.frame.pack()
+        self.Q1 = QG.getQuestion(number)
+        self.Q1A = QG.getAnswersShuffled(number)
+        self.strVar = StringVar()
+        labelStr = Label(self.frame, textvariable=self.strVar, relief=RAISED)
+        self.strVar.set(QG.getQuestion(number))
+        labelStr.pack()
+
+        self.intVar = StringVar()
+        R1 = Radiobutton(self.frame, text=str(self.Q1A[0]), variable=self.intVar, value=str(self.Q1A[0]))
+        R1.pack(anchor=W)
+        R2 = Radiobutton(self.frame, text=str(self.Q1A[1]), variable=self.intVar, value=str(self.Q1A[1]))
+        R2.pack(anchor=W)
+        R3 = Radiobutton(self.frame, text=str(self.Q1A[2]), variable=self.intVar, value=str(self.Q1A[2]))
+        R3.pack(anchor=W)
+        R4 = Radiobutton(self.frame, text=str(self.Q1A[3]), variable=self.intVar, value=str(self.Q1A[3]))
+        R4.pack(anchor=W)
+
+        submit = Button(self.frame, text="Submit", command=self.selr)
+        submit.pack()
+
+        self.labelOpt = Label(self.frame)
+        self.labelOpt.pack()
+
+
+
+    def selr(self):
+        answer = QG.getCorrectAnswer(self.number)
+        if answer == self.intVar.get():
+            answerText = "You are correct"
+        else:
+            answerText = "You are Wrong"
+        self.labelOpt.config(text=answerText)
+
+
+def data():
+    Quest().main(1)
+    Quest().main(2)
+    Quest().main(3)
+    Quest().main(4)
+    Quest().main(5)
+    randomQuest().main()
+    randomQuest().main()
+    randomQuest().main()
+    randomQuest().main()
+    randomQuest().main()
 
 
 root = Tk()
-
 root.title("Counting Test")
 
-test().main()
 
+
+data()
 
 
 root.mainloop()
