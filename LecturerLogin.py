792d1c08f9f437571676acff2defbcca9f0a5182
joshsaul1996 <joshsaul1996@gmail.com>
2016-04-14 13:19:06 +0100

    Updated Lecturer Login

--- LecturerLogin.py
+++ LecturerLogin.py
@@ -1,5 +1,5 @@
 from pymysql import connect,err,sys,cursors
 conn=connect("csmysql.cs.cf.ac.uk",user='c1512900',passwd='uWC3hneR',db='c1512900')
 cursor=conn.cursor()
-cursor.execute("SELECT * FROM Lecturer where password = ?")
+cursor.execute("SELECT * FROM Lecturer where Pass = ?")
 data=cursor.fetchone()
