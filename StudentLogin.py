9a2f26e59da9d61fcf36f928429467cdf6a8ada2
joshsaul1996 <joshsaul1996@gmail.com>
2016-04-14 13:18:18 +0100

    Updated Student Login

--- StudentLogin.py
+++ StudentLogin.py
@@ -1,5 +1,5 @@
 from pymysql import connect,err,sys,cursors
 conn=connect("csmysql.cs.cf.ac.uk",user='c1512900',passwd='uWC3hneR',db='c1512900')
 cursor=conn.cursor()
-cursor.execute("SELECT * FROM Student where password = ?")
+cursor.execute("SELECT * FROM Student where Pass = ?")
 data=cursor.fetchone()
